1a0b79ce7e72e949eaaa08c1b5eaa58956a8d3a6
Pawan <pawan@BLL008663.local>
2018-07-26 07:57:22 +0530

    implementation: simple queue using linked list in swift

---
+++ queue/swift/queue_using_linked_list.swift
@@ -0,0 +1,57 @@
+class LinkedListNode<E> {
+    var next: LinkedListNode<E>? = nil
+    var data: E? = nil
+
+    init(data: E) {
+        self.data = data
+    }
+}
+
+struct Queue<E> {
+
+    var head: LinkedListNode<E>? = nil
+    var tail: LinkedListNode<E>? = nil
+
+    mutating func enqueue(element: E) {
+        let newNode = LinkedListNode<E>(data: element)
+        if tail == nil {
+            head = newNode
+            tail = newNode
+        } else {
+            tail?.next = newNode
+            tail = newNode
+        }
+    }
+
+    mutating func dequeue() -> E? {
+        if head != nil {
+            let temp = head?.data
+            head = head?.next
+            return temp
+        }
+        return nil
+    }
+
+    func printQueue() {
+        var temp = head
+        while temp != nil {
+            if let value = temp?.data {
+                print(value, separator: " ", terminator: "->")
+            }
+            temp = temp?.next
+        }
+        print()
+    }
+}
+
+var queue = Queue<Int>()
+for item in [29, 73, 99, 28, 46, 10, 42, 93, 64, 56, 82, 19, 83] {
+    queue.enqueue(element: item)
+}
+queue.printQueue()
+_ = queue.dequeue()
+_ = queue.dequeue()
+_ = queue.dequeue()
+_ = queue.dequeue()
+
+queue.printQueue()
